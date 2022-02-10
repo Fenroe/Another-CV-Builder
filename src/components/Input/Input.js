15d4156d6ce89184cd5a3ec9320b857dad5b7932
Fenroe <scrambles9494@gmail.com>
2022-02-10 21:08:43 +0200

    input shelled

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,18 +3,32 @@ function Input() {
         <div>
             <fieldset>
                 <legend>Personal Details</legend>
+                <label>First Name</label>
+                <input />
+                <label>Last Name</label>
+                <input />
+                <label>Email Adress</label>
+                <input />
+                <label>Phone Number</label>
+                <input />
+                <label>Website</label>
+                <input />
             </fieldset>
             <fieldset>
                 <legend>Statement</legend>
+                <textarea />
             </fieldset>
             <fieldset>
                 <legend>Key Skills</legend>
+                <button>Add Skill</button>
             </fieldset>
             <fieldset>
                 <legend>Employment History</legend>
+                <button>Add Employer</button>
             </fieldset>
             <fieldset>
                 <legend>Education</legend>
+                <button>Add Institute</button>
             </fieldset>
         </div>
     )
